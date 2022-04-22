5c9e793950fd683283d84ea0ba3928ffae4c2135
Rachel Bock <rachel.online@gmail.com>
2022-04-21 18:47:04 -0600

    Features/Remove unused class

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 // Query Selectors
 var ticTacToeBoard = document.querySelector('.game-board');
+var currentPlayer = document.querySelector('.current-player');
+var heading = document.querySelector('.heading');
+var playerOneWins = document.querySelector('.player-one-wins');
+var playerTwoWins = document.querySelector('.player-two-wins');
+
 
 // Global variables
 var ourGame = new Game();
@@ -9,14 +14,10 @@ ticTacToeBoard.addEventListener('click', selectSquare);
 
 // DOM Functions
 function renderHeading() {
-  var currentPlayer = document.querySelector('.current-player');
   currentPlayer.innerText = ourGame.currentPlayer;
 }
 
 function renderWinner() {
-  var heading = document.querySelector('.heading');
-  var playerOneWins = document.querySelector('.player-one-wins');
-  var playerTwoWins = document.querySelector('.player-two-wins');
   heading.innerHTML = ourGame.showGameOver();
   playerOneWins.innerText = ourGame.firstPlayer.wins;
   playerTwoWins.innerText = ourGame.secondPlayer.wins;
@@ -24,46 +25,43 @@ function renderWinner() {
 
 function selectSquare(event) {
   var square = document.querySelector(`#${event.target.id}`);
-  square.classList.remove('board-square-unused');
+  // square.classList.remove('board-square-unused');
   switch (event.target.id) {
     case 'one':
       var move = ourGame.makeMove(1);
-      console.log(move);
       break;
     case 'two':
       var move = ourGame.makeMove(2);
-      console.log(move);
       break;
     case 'three':
       var move = ourGame.makeMove(3);
-      console.log(move);
       break;
     case 'four':
       var move = ourGame.makeMove(4);
-      console.log(move);
       break;
     case 'five':
       var move = ourGame.makeMove(5);
-      console.log(move);
       break;
     case 'six':
       var move = ourGame.makeMove(6);
-      console.log(move);
       break;
     case 'seven':
       var move = ourGame.makeMove(7);
-      console.log(move);
       break;
     case 'eight':
       var move = ourGame.makeMove(8);
-      console.log(move);
       break;
     case 'nine':
       var move = ourGame.makeMove(9);
-      console.log(move);
       break;                                                                                          
     default:
       break;
   }   
+  console.log(move, '<<< move');
+  if(move) {
+    square.innerText = ourGame.currentPlayer;
+    
+  }
+  
 }
 
