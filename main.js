ccfd848231ca373e3cc7f29f8d4ee66ace703950
Rachel Bock <rachel.online@gmail.com>
2022-04-21 19:47:21 -0600

    Features/Add query selectors to main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+var gameBoard = document.querySelector('.game-board');
+var heading = document.querySelector('.heading');
+var playerOneWins = document.querySelector('.player-one-wins');
+var playerTwoWins = document.querySelector('.player-two-wins');
