4c511678e51d3ba1d407896003d87704498bf22f
Rachel Bock <rachel.online@gmail.com>
2022-04-21 19:51:28 -0600

    Features/Add event listener and event handler function

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
+// QUERY SELECTORS
 var gameBoard = document.querySelector('.game-board');
 var heading = document.querySelector('.heading');
 var playerOneWins = document.querySelector('.player-one-wins');
 var playerTwoWins = document.querySelector('.player-two-wins');
+
+// EVENT LISTENERS
+gameBoard.addEventListener('click', selectSquare);
+
+// FUNCTIONS
+function selectSquare(event) {
+  
+}
