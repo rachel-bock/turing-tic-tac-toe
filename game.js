1caac12dbde27ccbbbb5d7ea5e27fd0fb2a8c736
Rachel Bock <rachel.online@gmail.com>
2022-04-21 19:09:23 -0600

    Features/Add Game Class properties

--- game.js
+++ game.js
@@ -4,15 +4,16 @@ class Game {
   constructor() {
     this.currentPlayer = 'X';
     this.gameBoard = new Array(10);
+    this.gameOver = false;
+    this.playerO = new Player(2, '0');
+    this.playerX = new Player(1, 'X');
+    this.playsFirst = 'X';
+    this.winner = null;
   }
 
-  checkForWinner() {
 
-  }
 
-  setupGame() {
 
-  }
 }
 
 module.exports = Game;
